176d062668730d29c7a86b8d5e699e6e1b556b10
yelim <choyr2016@naver.com>
2021-04-21 10:20:51 +0900

    [ADD] rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App()
- {
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({movies, isLoading: false});
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
     return (
-      <>
-        <span>조예림 ㄹㅇ 마렵네</span>
-      </>
+      <div>
+        {isLoading 
+          ? "Loading..." 
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                />
+              ))}
+      </div>
     );
- }
+  }
+}
 
- export default App;
+export default App;
